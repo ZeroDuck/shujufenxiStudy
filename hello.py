9a0c2c3c9958dbbe5f0f236a2f21c6f3a84b4b0b
ZeroDuck <2536650423@qq.com>
2020-07-09 18:02:20 +0800

    test

---
+++ hello.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+@Filename      :hello.py
+@Function      :
+@Time          :2020/07/09 17:33:28
+@Author        :Zero
+@Version       :1.0
+@software      :VScode
+'''
+
+print("HelloWorld!!! ZXj")
